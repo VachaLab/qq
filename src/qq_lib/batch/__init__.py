3c492b2cf52ba9a6625f2d86fe92100e7b0546b9
Ladislav Bartoš <ladmeb@gmail.com>
2026-05-04 22:13:37 +0200

    Batch systems registration

--- src/qq_lib/batch/__init__.py
+++ src/qq_lib/batch/__init__.py
@@ -9,3 +9,11 @@ schedulers. It defines the abstract interfaces for jobs, queues, and nodes,
 together with the concrete backends for PBS, Slurm, and site-specific Slurm
 variants.
 """
+
+# import so that these batch systems are available but do not export them from here
+from .pbs import PBS as _PBS
+from .slurm import Slurm as _Slurm
+from .slurmit4i import SlurmIT4I as _SlurmIT4I
+from .slurmlumi import SlurmLumi as _SlurmLumi
+
+_PBS, _Slurm, _SlurmIT4I, _SlurmLumi
